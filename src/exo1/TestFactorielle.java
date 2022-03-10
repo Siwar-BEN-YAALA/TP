bd3684ce4b4ad6c4b73971adbc528e15dba4a83a
Siwar BEN YAALA <siwarbenyaala99@gmail.com>
2022-03-10 20:37:31 +0100

    Created TestFactorielle class

---
+++ src/exo1/TestFactorielle.java
@@ -0,0 +1,11 @@
+package exo1;
+
+public class TestFactorielle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Factorielle fact = new Factorielle();
+		System.out.println("Factorielle de 10 = " + fact.factorielle(10));
+	}
+
+}
